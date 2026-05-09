3fac1c9d7e76721e6606e0d8342820cb146933f6
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2026-05-09 03:06:36 +0300

    chore: update MQTT broker IP address to 192.168.1.9

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // MQTT Configuration
-  static const String mqttBrokerIp = '192.168.1.5';
+  static const String mqttBrokerIp = '192.168.1.9';
   static const int mqttPort = 1883;
   static const String mqttClientId = 'flutter_client';
 
@@ -10,11 +10,11 @@ class AppConstants {
   static const String topicHumidity = 'smartclassroom/humidity';
   static const String topicCurrentMode = 'smartclassroom/current_mode';
   static const String topicLightStatus = 'smartclassroom/light_status';
-  
+
   // Publish Topics
   static const String topicModeControl = 'smartclassroom/mode';
   static const String topicLightControl = 'smartclassroom/light';
-  
+
   // Topic Filter (Subscription)
   static const String topicFilter = 'smartclassroom/#';
 }
